453251fd2e04c266274f2f676dfa4e574125923e
pasichmaria <pasichmaria@gmail.com>
2022-01-19 22:52:43 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        // объявление типа делегата CalcFigure
+        delegate double CalcFigure(double r);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // объявление статических методов в классе
+        // длина окружности
+        public static double Get_Length(double r)
+        {
+            double length;
+            length = 2 * 3.1415 * r;
+            return length;
+        }
+
+        // площадь круга
+        public static double Get_Area(double r)
+        {
+            double area;
+            area = 3.1415 * r * r;
+            return area;
+        }
+
+        // объем шара
+        public static double Get_Volume(double r)
+        {
+            double volume;
+            volume = 3.1415 * r * r * r * 4.0 / 3.0;
+            return volume;
+        }
+
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(44, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = " Посчитать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(44, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(96, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите радиус ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Длина окружности";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Площадь круга";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Обьем шара";
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+                double radius, length, area, volume;
+
+                // считать радиус
+                radius = Convert.ToDouble(textBox1.Text);
+
+                // вызов делегата
+                // сконструировать делегат
+                CalcFigure CF = new CalcFigure(Get_Length);
+
+                length = CF(radius); // 1. Вызов метода Get_Length()
+
+                CF = new CalcFigure(Get_Area);
+                area = CF(radius); // 2. Вызов метода Get_Area()
+
+                CF = new CalcFigure(Get_Volume);
+                volume = CF(radius); // 3. Вызов метода Get_Volume()
+
+                // вывод результатов на форму
+                label2.Text = "Длина окружности = " + length.ToString();
+                label3.Text = "Площадь круга = " + area.ToString();
+                label4.Text = "Объем шара = " + volume.ToString();
+            }
+    }
+    }
+    
